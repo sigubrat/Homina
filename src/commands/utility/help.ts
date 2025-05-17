d6cc4e29a4e3bdf0e8ffccd7e0d33f39fb5dd294
sigubrat <sigurd.brattland@fangstr.no>
2025-05-17 21:00:04 +0200

    Hotfix: Fix help breaking after too many commands being added :ambulance:

--- src/commands/utility/help.ts
+++ src/commands/utility/help.ts
@@ -1,10 +1,10 @@
 import { logger } from "@/lib";
 import {
     ChatInputCommandInteraction,
-    EmbedBuilder,
     MessageFlags,
     SlashCommandBuilder,
 } from "discord.js";
+import { Pagination } from "pagination.djs";
 
 export const cooldown = 5; // Cooldown in seconds
 
@@ -13,37 +13,49 @@ export const data = new SlashCommandBuilder()
     .setDescription("Get an overview of the bot commands");
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+    try {
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
-    // @ts-expect-error - This works because our client is extended with commands (see index.ts in root)
-    const commands = interaction.client.commands.map((command: any) => {
-        return `* \`/${command.data.name}\` - ${command.data.description}`;
-    });
-
-    const embed = new EmbedBuilder()
-        .setColor("#0099ff")
-        .setTitle("Help")
-        .setDescription(
-            "Useful information about the bot and its commands.\n\n"
-        )
-        .addFields(
-            {
-                name: "For new users",
-                value: "1. Register your account using `/register`.\n2. Use `/get-member-ids` to get a list of members in the guild.\n3. Use the list to register usernames.\n",
-            },
-            {
-                name: "Commands",
-                value: commands.join("\n"),
-            }
-        )
-        .setTimestamp()
-        .setFooter({
-            text: "Bot Help",
+        // @ts-expect-error - This works because our client is extended with commands (see index.ts in root)
+        const commands = interaction.client.commands.map((command: any) => {
+            return [command.data.name, command.data.description];
         });
 
-    await interaction.editReply({
-        embeds: [embed],
-    });
+        const pagination = new Pagination(interaction, {
+            limit: 10,
+        })
+            .setColor("#0099ff")
+            .setTitle("Help")
+            .setDescription(
+                "Useful information about the bot and its commands.\n\n"
+            )
+            .addFields(
+                {
+                    name: "For new users",
+                    value: "1. Register your account using `/register`.\n2. Use `/get-member-ids` to get a list of members in the guild.\n3. Use the list to register usernames.\n",
+                },
+                {
+                    name: "__Commands:__",
+                    value: "",
+                }
+            )
+            .setTimestamp()
+            .setFooter({
+                text: "Bot Help",
+            });
 
-    logger.info(`${interaction.user.username} used /help`);
+        for (const command of commands) {
+            pagination.addFields({
+                name: command[0],
+                value: command[1],
+            });
+        }
+
+        pagination.paginateFields(true);
+        await pagination.render();
+
+        logger.info(`${interaction.user.username} used /help`);
+    } catch (error) {
+        console.error(error);
+    }
 }
