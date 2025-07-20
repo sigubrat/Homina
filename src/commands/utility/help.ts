98198d4dc23af6b9ecd4541813972f1d3db1b59d
Sigurd Brattland <47779602+sigubrat@users.noreply.github.com>
2025-07-20 18:37:13 +0200

    0.26.0 Ease of use and more rarity filter (#30)
    * Add rarity filter to season-participation
    
    * Add info the meta team distribution and improve help command
    
    * Bump version and add changelog
    
    ---------

--- src/commands/utility/help.ts
+++ src/commands/utility/help.ts
@@ -1,5 +1,11 @@
 import { logger } from "@/lib";
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ChatInputCommandInteraction,
+    SlashCommandBuilder,
+} from "discord.js";
 import { Pagination } from "pagination.djs";
 
 export const cooldown = 5; // Cooldown in seconds
@@ -27,8 +33,25 @@ export async function execute(interaction: ChatInputCommandInteraction) {
             )
             .addFields(
                 {
-                    name: "For new users",
-                    value: "1. Register your account using `/register` If someone in your guild has already registered your usernames you are now ready. If not, see step 2.\n2. Use `/member-ids` to get a list of members in the guild.\n3. Use the list and `/update-members` command to register usernames.\n",
+                    name: "First time setup for your guild",
+                    value:
+                        "1. Register your account using `/register`. You will need an API created by a (co-)leader in your guild with Guild and Raid scope checked off.\n" +
+                        "2. Use `/member-ids` to get a list of members in the guild.\n" +
+                        "3. Use the list and `/update-members` command to register usernames.\n",
+                },
+                {
+                    name: "Register additional users",
+                    value:
+                        "1. If others in the guild want to use the bot they will have to register using the `/register` command.\n" +
+                        "2. They will not need to update member IDs as that is shared by all guild members. Ready to go!\n" +
+                        "Note: They must also register with an API key created by a (co-)leader in the guild with Guild and Raid scope checked off.\n",
+                },
+                {
+                    name: "Updating members",
+                    value:
+                        "Members come and go, but that means you will need to update the member IDs stored in the bot. You have two options:\n" +
+                        "1. Use `/member-ids`and add the username of the new member before using `/update-members`.\n" +
+                        "2. Use `/add-member`or /remove-member` to add or remove a single member.\n",
                 },
                 {
                     name: "__Commands:__",
@@ -48,6 +71,26 @@ export async function execute(interaction: ChatInputCommandInteraction) {
         }
 
         pagination.paginateFields(true);
+
+        const githubBtn = new ButtonBuilder()
+            .setLabel("GitHub")
+            .setStyle(ButtonStyle.Link)
+            .setEmoji("<:github:1396463516835385446>")
+            .setURL("https://github.com/sigubrat/Homina");
+
+        const docsBtn = new ButtonBuilder()
+            .setLabel("Support server")
+            .setStyle(ButtonStyle.Link)
+            .setEmoji("<:homina:1393217807172239496>")
+            .setURL("https://discord.gg/FajYxuWY9b");
+
+        pagination.addActionRows([
+            new ActionRowBuilder<ButtonBuilder>().addComponents(
+                githubBtn,
+                docsBtn
+            ),
+        ]);
+
         await pagination.render();
 
         logger.info(`${interaction.user.username} used /help`);
