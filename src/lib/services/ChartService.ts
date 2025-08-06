8107e7820e62e70a2a5dd0376e999478dc247e66
Sigurd Brattland <47779602+sigubrat@users.noreply.github.com>
2025-08-06 09:34:36 +0200

    Update chart label for prime damage portion (#38)
    * Update chart label for prime damage portion
    
    * Update changelog for version 0.31.1, clarify damage descriptions in season-by-rarity, and adjust chart label formatting
    
    ---------
    
    Co-authored-by: sigubrat <sigurd.brattland@fangstr.no>

--- src/lib/services/ChartService.ts
+++ src/lib/services/ChartService.ts
@@ -267,13 +267,13 @@ export class ChartService {
                                 size: 11,
                             },
                             formatter: function (value: number) {
-                                return shortenNumber(value);
+                                return value > 0 ? shortenNumber(value) : null;
                             },
                         },
                     },
                     {
                         backgroundColor: CHART_COLORS.purple,
-                        label: "Prime damage portion",
+                        label: "Prime damage",
                         data: primeDamage,
                         borderWidth: 1,
                         datalabels: {
@@ -303,7 +303,7 @@ export class ChartService {
                                 size: 11,
                             },
                             formatter: function (value: number) {
-                                return shortenNumber(value);
+                                return value > 0 ? shortenNumber(value) : null;
                             },
                         },
                     },
