ca0ac01563ca1e1a19c61ccc26381d84465090b4
sigubrat <sigurd.brattland@fangstr.no>
2026-01-09 14:52:41 +0100

    feat: invite user

---
+++ src/commands/guild-management/inviteUser.ts
@@ -0,0 +1,83 @@
+import { dbController, logger } from "@/lib";
+import { MessageService } from "@/lib/services/MessageService";
+import { isValidUUIDv4 } from "@/lib/utils/mathUtils";
+import {
+    ChatInputCommandInteraction,
+    MessageFlags,
+    SlashCommandBuilder,
+} from "discord.js";
+
+// 5 minutes cooldown
+export const cooldown = 5 * 60;
+
+export const data = new SlashCommandBuilder()
+    .setName("invite-user")
+    .setDescription("Invite a user to register with your API token")
+    .addUserOption((option) =>
+        option
+            .setName("user")
+            .setDescription("The user to invite")
+            .setRequired(true)
+    );
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+    await interaction.deferReply();
+
+    const commandCallerId = interaction.user.id;
+    const invitedUser = interaction.options.getUser("user", true);
+    const commandCallerUsername = interaction.user.username;
+
+    if (invitedUser.id === commandCallerId) {
+        await interaction.editReply({
+            content: "You cannot invite yourself.",
+            options: {
+                flags: MessageFlags.Ephemeral,
+            },
+        });
+        return;
+    }
+
+    try {
+        // Fetch the command caller's API token
+        const apiToken = await dbController.getUserToken(commandCallerId);
+        if (!apiToken || !isValidUUIDv4(apiToken)) {
+            await interaction.editReply({
+                content:
+                    "Could not find a valid token registered to your discord account. Please register first using /register or reach out to the support server if something is wrong.",
+                options: {
+                    flags: MessageFlags.Ephemeral,
+                },
+            });
+            return;
+        }
+
+        // Invite the user by sending them a DM for confirmation. No user should be registered without their consent.
+        const messageService = new MessageService(interaction.client);
+        const invitedSuccessfully = await messageService.inviteUser(
+            invitedUser,
+            commandCallerUsername,
+            apiToken
+        );
+
+        if (invitedSuccessfully) {
+            await interaction.editReply({
+                content: `Successfully sent an invitation to <@${invitedUser.id}>. They need to confirm the invitation via DM to complete the registration.`,
+            });
+        } else {
+            await interaction.editReply({
+                content: `Failed to send an invitation to <@${invitedUser.id}>. They might have DMs disabled. Please ask them to enable DMs and try again.`,
+            });
+        }
+    } catch (error) {
+        await interaction.editReply({
+            content:
+                "An error occurred while trying to invite the user. Please try again later.",
+            options: {
+                flags: MessageFlags.Ephemeral,
+            },
+        });
+        logger.error(
+            `Error while ${interaction.user.username} was trying to invite ${invitedUser.username}: ${error}`
+        );
+    }
+}
--- src/lib/services/MessageService.ts
+++ src/lib/services/MessageService.ts
@@ -1,5 +1,17 @@
-import { Client } from "discord.js";
-import { logger } from "@/lib";
+import {
+    ActionRowBuilder,
+    AttachmentBuilder,
+    ButtonBuilder,
+    ButtonInteraction,
+    ButtonStyle,
+    Client,
+    EmbedBuilder,
+    MessageFlags,
+    User,
+} from "discord.js";
+import { dbController, logger } from "@/lib";
+import { isValidUUIDv4 } from "../utils/mathUtils";
+import path from "path";
 
 export class MessageService {
     private client: Client;
@@ -31,6 +43,163 @@ export class MessageService {
                 error,
                 `Failed to send deletion alert to user: ${userId}`
             );
+        }
+    }
+
+    public async inviteUser(
+        user: User,
+        inviterName: string,
+        apiToken: string
+    ): Promise<boolean> {
+        const userId = user.id;
+        try {
+            // Create an embed with a confirm and decline button
+            const imagePath = path.join(
+                __dirname,
+                "../../../docs/img/homina-logo.png"
+            );
+            const attachment = new AttachmentBuilder(imagePath, {
+                name: "homina-logo.png",
+            });
+
+            const embed = new EmbedBuilder()
+                .setTitle("Invitation to Register with Homina Bot")
+                .setThumbnail("attachment://homina-logo.png")
+                .setDescription(
+                    `${inviterName} has invited you to register with the Homina Discord bot using their API token.\n` +
+                        `Registering allows you to access various features and functionalities provided by the bot.\n\n` +
+                        `Press confirm to register with their token or decline the invitation.\n\n` +
+                        `Note: if you do not recognise this invitation, you can safely ignore this message.`
+                )
+                .setColor("#0099ff")
+                .setTimestamp();
+
+            const confirmButton = new ButtonBuilder()
+                .setCustomId(`invite_confirm_${userId}_${apiToken}`)
+                .setLabel("Confirm")
+                .setStyle(ButtonStyle.Success);
+
+            const declineButton = new ButtonBuilder()
+                .setCustomId(`invite_decline_${userId}`)
+                .setLabel("Decline")
+                .setStyle(ButtonStyle.Danger);
+
+            const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+                confirmButton,
+                declineButton
+            );
+
+            await user.send({
+                embeds: [embed],
+                components: [row],
+                files: [attachment],
+            });
+            logger.info(`Successfully sent invitation to user: ${userId}`);
+            return true;
+        } catch (error) {
+            logger.error(error, `Failed to send invitation to user: ${userId}`);
+            return false;
+        }
+    }
+
+    public async handleInviteConfirm(
+        interaction: ButtonInteraction,
+        apiToken: string
+    ): Promise<void> {
+        await interaction.deferUpdate();
+
+        try {
+            const clickedConfirmButton = new ButtonBuilder()
+                .setCustomId("disabled_confirm")
+                .setLabel("Confirmed")
+                .setStyle(ButtonStyle.Secondary)
+                .setDisabled(true);
+
+            const disabledDeclineButton = new ButtonBuilder()
+                .setCustomId("disabled_decline")
+                .setLabel("Decline")
+                .setStyle(ButtonStyle.Danger)
+                .setDisabled(true);
+
+            await interaction.message.edit({
+                components: [
+                    new ActionRowBuilder<ButtonBuilder>().addComponents(
+                        clickedConfirmButton,
+                        disabledDeclineButton
+                    ),
+                ],
+            });
+
+            await interaction.followUp({
+                content:
+                    "You have confirmed the invitation. Registration processing...",
+            });
+            logger.info(`User ${interaction.user.id} confirmed invitation`);
+
+            // Register the user with the provided API token
+            if (!apiToken || !isValidUUIDv4(apiToken)) {
+                await interaction.followUp({
+                    content:
+                        "Invalid API token provided. Could not register you. Please reach out to the one who invited you or the support server.",
+                });
+                return;
+            }
+
+            const result = await dbController.registerUser(
+                interaction.user.id,
+                apiToken
+            );
+            if (!result) {
+                await interaction.followUp({
+                    content:
+                        "Something went wrong while registering you. Please try again later or reach out to the support server.",
+                });
+                return;
+            }
+
+            await interaction.followUp({
+                content:
+                    "You have been successfully registered! You can now use the bot's features.",
+            });
+
+            return;
+        } catch (error) {
+            logger.error(
+                `Error registering user ${interaction.user.id}: ${error}`
+            );
+            await interaction.followUp({
+                content:
+                    "An error occurred while registering you. Please try again later or reach out to the support server.",
+                options: { flags: MessageFlags.Ephemeral },
+            });
+        }
+    }
+
+    public async handleInviteDecline(
+        interaction: ButtonInteraction
+    ): Promise<void> {
+        await interaction.deferUpdate();
+
+        try {
+            await interaction.message.edit({
+                components: [],
+            });
+
+            await interaction.followUp({
+                content: "You have declined the invitation.",
+                embeds: [],
+                components: [],
+            });
+            logger.info(`User ${interaction.user.id} declined invitation`);
+        } catch (error) {
+            logger.error(
+                `Error handling decline for user ${interaction.user.id}: ${error}`
+            );
+            await interaction.followUp({
+                content:
+                    "An error occurred while processing your decline. Please try again later.",
+                options: { flags: MessageFlags.Ephemeral },
+            });
         }
     }
 }
