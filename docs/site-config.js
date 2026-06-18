b133c6b885fd9acdfca594ed6b672f41b52a1c3f
sigubrat <sigurd.brattland@fangstr.no>
2026-06-18 12:02:31 +0200

    feat: add automatic hiding of code banner based on date

--- docs/site-config.js
+++ docs/site-config.js
@@ -5,12 +5,23 @@ var SITE_CONFIG = {
     // Temporary code banner
     showBanner: true,
     tempCode: "HOMINA6K",
-    tempNote: "Limited-time code \u2013 redeem in-game before it expires."
+    tempNote: "Limited-time code \u2013 redeem in-game before it expires.",
+    // ISO date string (YYYY-MM-DD) after which the banner auto-hides. Set to null to disable expiry.
+    bannerExpiryDate: "2026-06-22"
 };
 
 (function () {
+    // Auto-hide banner if past expiry date
+    var bannerVisible = SITE_CONFIG.showBanner;
+    if (bannerVisible && SITE_CONFIG.bannerExpiryDate) {
+        var expiry = new Date(SITE_CONFIG.bannerExpiryDate + "T00:00:00");
+        if (Date.now() >= expiry.getTime()) {
+            bannerVisible = false;
+        }
+    }
+
     // Apply banner config
-    if (SITE_CONFIG.showBanner) {
+    if (bannerVisible) {
         document.body.classList.add("fx-tempcode");
     } else {
         document.body.classList.remove("fx-tempcode");
