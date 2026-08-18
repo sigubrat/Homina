8a46f3dd6bae09c8f96f87b516f55260b5669c33
sigubrat <sigurd.brattland@fangstr.no>
2026-08-18 14:07:08 +0200

    chore: update STANDARD_FOOTER_TEXT to include raffle code

--- src/lib/configs/constants.ts
+++ src/lib/configs/constants.ts
@@ -9,7 +9,8 @@ export const getCurrentSeason = () => calculateCurrentSeason(new Date());
 export const META_TEAM_THRESHOLD = 5;
 export const MAX_TOKENS_PER_SEASON = 28;
 
-export const STANDARD_FOOTER_TEXT = "Referral code: HUG-44-CAN";
+export const STANDARD_FOOTER_TEXT =
+    "Referral code: HUG-44-CAN | Raffle code: AUTOMATON";
 
 // Scheduled commands configuration
 export const MAX_SCHEDULES_PER_GUILD = 10;
